5e2ba912338fc108f1587d902cde98f9732df022
Sonic <142159825+sonicc97@users.noreply.github.com>
2024-02-05 19:31:45 +0100

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    public AudioSource audioPlayer;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Getting the component that is bound to the object
+        playerRB = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        audioPlayer = GetComponent<AudioSource>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            audioPlayer.PlayOneShot(jumpSound, 1.0f);
+        }
+    }
+    //Is called when it collides with another object, in this case "ground"
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+            dirtParticle.Play();
+        }
+        else if(collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("GameOver");
+            gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            //pPrticleSystem - enables placement in unity editor
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            audioPlayer.PlayOneShot(crashSound, 1.0f);
+        }
+    }
+}
